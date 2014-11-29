b969936cc051b6b5204c0d1203bfa800eb8913d3
Hasan Arous <arous.hasan@gmail.com>
2014-11-29 22:06:11 +0200

    better checking for whether we should load the coffee or the js version.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var coffeeScriptAvailable = true;
 
 try { // Development time...
 	require('coffee-script/register')
-	module.exports = require('./src/')
 } catch (e) {
+	coffeeScriptAvailable = false;
 	//console.log('Falling back to the compiled javascript version', e.message, e.stack, e)
-	module.exports = require('./lib/')
 }
+
+module.exports = require(coffeeScriptAvailable? './src/': './lib/')
