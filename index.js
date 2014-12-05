50bddc998021e99b1a56b95e8f87c56b69ae7388
Hasan Arous <arous.hasan@gmail.com>
2014-12-05 15:11:48 +0200

    plastics

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ try { // Development time...
 	//console.log('Falling back to the compiled javascript version', e.message, e.stack, e)
 }
 
-module.exports = require(coffeeScriptAvailable? './src/': './lib/')
+module.exports = require(coffeeScriptAvailable? './src/': './lib/');
